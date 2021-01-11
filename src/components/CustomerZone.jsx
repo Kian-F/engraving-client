0a3c8abbab15702688e3aa39aed4ac7735f16536
Kian-F <52603628+Kian-F@users.noreply.github.com>
2021-01-11 23:10:41 +1100

    update

--- src/components/CustomerZone.jsx
+++ src/components/CustomerZone.jsx
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
-import SignIn from './SignIn';
-import Nav from './Nav';
-import SignUp from './SignUp';
-
-
+import React, { Component } from "react";
+import SignIn from "./SignIn";
+import Nav from "./Nav";
+import SignUp from "./SignUp";
 
 class CustomerZone extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>
-                <Nav/>
-                <SignIn history={this.props.history}/>
-                <SignUp history={this.props.history}/>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <SignIn history={this.props.history} />
+        <SignUp history={this.props.history} />
 
-                <h1></h1>
-            </div>
-         );
-    }
+        <h1></h1>
+      </div>
+    );
+  }
 }
- 
+
 export default CustomerZone;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,72 +1,75 @@
-import React, { Component } from 'react';
-import { Accordion, Card, Button } from 'react-bootstrap';
-import Editor from './Editor';
+import React, { Component } from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
+import Editor from "./Editor";
 import "../index.css";
-
-
-
-
 
 const SERVER_URL = "http://localhost:3000/products/index";
 
-
 class SideBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Shapes:'',
-            inputText:''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      Shapes: "",
+      inputText: "",
+    };
+  }
 
-    // handleTextInput = (text) => {
-    //     this.setState({ inputText: text}); 
-    //     console.log(this.state.inputText);
-        
-    // }
-    
-    render() {
-        return (<div>
-            
-            <Accordion className="according" defaultActiveKey="0">
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            Shapes
-      </Accordion.Toggle>
-                    </Card.Header>
+  // handleTextInput = (text) => {
+  //     this.setState({ inputText: text});
+  //     console.log(this.state.inputText);
 
-                    <Accordion.Collapse eventKey="0">
-                    <Card.Img className="rect" src="https://sdo.textalk.se/img/preview/rectangle.png" />
-                    </Accordion.Collapse>
-                    <Accordion.Collapse eventKey="0">  
-                    <Card.Img className="rounded" src="https://sdo.textalk.se/img/preview/rectangle_rounded_corners.png"></Card.Img>
-                    </Accordion.Collapse>
-                    <Accordion.Collapse eventKey="0">  
-                    <Card.Img className="oval" src="https://sdo.textalk.se/img/preview/ellipse.png"></Card.Img>
-                    </Accordion.Collapse>
+  // }
 
-                </Card>
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                            Text
-      </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="1">
-                        <Card.Body className="editor"><Editor classNamae="editore"onTextSubmit={this.props.handleTextInput} onSizeChange={this.props.handleSize}/></Card.Body>
-                    </Accordion.Collapse>
-                    
-                    
-                </Card>
-            </Accordion>
-            
-            
-        </div>);
-    }
+  render() {
+    return (
+      <div>
+        <Accordion className="according" defaultActiveKey="0">
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Shapes
+              </Accordion.Toggle>
+            </Card.Header>
 
+            <Accordion.Collapse eventKey="0">
+              <Card.Img
+                className="rect"
+                src="https://sdo.textalk.se/img/preview/rectangle.png"
+              />
+            </Accordion.Collapse>
+            <Accordion.Collapse eventKey="0">
+              <Card.Img
+                className="rounded"
+                src="https://sdo.textalk.se/img/preview/rectangle_rounded_corners.png"
+              ></Card.Img>
+            </Accordion.Collapse>
+            <Accordion.Collapse eventKey="0">
+              <Card.Img
+                className="oval"
+                src="https://sdo.textalk.se/img/preview/ellipse.png"
+              ></Card.Img>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                Text
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body className="editor">
+                <Editor
+                  className="editore"
+                  onTextSubmit={this.props.handleTextInput}
+                  onSizeChange={this.props.handleSize}
+                />
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </div>
+    );
+  }
 }
-
-
 
 export default SideBar;
